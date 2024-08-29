a3a61ee23c27e31021c3d273760403b5d7b818dd
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-29 14:16:59 +0000

    update icon to add post

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = () => {
       activeClassName={styles.Active}
       to="/posts/create"
     >
-      <i className="far fa-plus-square"></i>Add post
+      <i className="fas fa-plus-square"></i>Add post
     </NavLink>
   );
 
