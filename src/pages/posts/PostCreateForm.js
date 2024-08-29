ac7a63f887de0a5fe1bc5af016b0c839fe7d2951
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-29 15:12:07 +0000

    add missed onclick function to button

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -105,7 +105,7 @@ function PostCreateForm() {
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
