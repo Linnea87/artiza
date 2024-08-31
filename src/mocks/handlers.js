dadc8fef7bb0d79f079ca75e4c7c864b949966a8
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-08-31 14:26:38 +0000

    runs automated tests on NavBar.js

---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const baseURL = "https://artiza-api-fbf88e8a2da5.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        "pk": 1,
+        "username": "Linnea",
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "profile_id": 1,
+        "profile_image": "https://res.cloudinary.com/delcgfh9h/image/upload/v1/media/images/IMG_0736_snkmsl"
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
